07354f731036e42e74d18d7fd8d76a41b510e74e
Punnoose <punnoosekuttyj@yahoo.com>
2018-04-06 04:45:25 -0400

    V1.0

---
+++ src/main/java/org/pk/level1/Level1Service.java
@@ -0,0 +1,5 @@
+package org.pk.level1;
+
+public interface Level1Service {
+	public void foo(int i);
+}
---
+++ src/main/java/org/pk/level1/Level1ServiceImpl.java
@@ -0,0 +1,7 @@
+package org.pk.level1;
+
+public class Level1ServiceImpl implements Level1Service {
+	public void foo(int i) {
+		System.out.println("Got i [" + i + "]");
+	}
+}
